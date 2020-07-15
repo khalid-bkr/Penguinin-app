1675bcd78f21eef42da0f597b6a36c85b13328c9
khalid abubaker <khalid7.bkr@gmail.com>
2020-07-15 17:05:16 +0300

    desgin modification

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,5 +4,8 @@ class CountriesController < ApplicationController
         @total_death = Country.order(death_count: :desc).first(6)
         @total_cases = Country.order(case_count: :desc).first(6)
         @total_recovery = Country.order(recovery_count: :desc).first(6)
+        @total_cases_sum = Country.sum(:case_count)
+        @total_death_sum = Country.sum(:death_count)
+        @total_recovery_sum = Country.sum(:recovery_count)
       end
 end
